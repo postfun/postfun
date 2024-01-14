da49f647abe8aaff3b5cba782c26d7a86cd95008
postfun <info@postfun.org>
2024-01-14 02:11:47 -0500

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    entry: './js/script.js', // Your main JavaScript file
+    output: {
+      filename: 'bundle.js', // Name of the bundled file
+      path: __dirname + '/dist', // Output directory
+    },
+  };
+  
