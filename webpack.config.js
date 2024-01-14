e48b9043632bf7be4b9f0197ca403b229fe84ae2
postfun <156542037+postfun@users.noreply.github.com>
2024-01-14 02:12:12 -0500

    Delete webpack.config.js

--- webpack.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    entry: './js/script.js', // Your main JavaScript file
-    output: {
-      filename: 'bundle.js', // Name of the bundled file
-      path: __dirname + '/dist', // Output directory
-    },
-  };
-  
