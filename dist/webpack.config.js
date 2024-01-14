c322957a26b4ca9f60e640f5e9cec4aab0ab623e
postfun <info@postfun.org>
2024-01-14 02:40:21 -0500

    Update webpack.config.js

--- dist/webpack.config.js
+++ dist/webpack.config.js
@@ -2,9 +2,9 @@ module.exports = {
   mode: 'development', // Or 'production'
   entry: './js/script.js',
   output: {
-    filename: 'dist/bundle.js',
-    path: __dirname + './dist',
-  },
+    filename: 'bundle.js', // Name of the bundled file
+    path: path.resolve(__dirname, 'dist'), // Output directory path
+  },  
   module: {
     rules: [
        {
